f694a41d3dd720cc1f53c1f5e509f8712c494e43
hailinh03 <hailinh2910203@gmail.com>
2025-01-26 14:40:15 +0700

    test titile

--- Login-Page-In-React-main/src/components/Login.jsx
+++ Login-Page-In-React-main/src/components/Login.jsx
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
 import Image from "../assets/image.png";
 import Logo from "../assets/logo.png";
-import GoogleSvg from "../assets/icons8-google.svg";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
-
   return (
     <div className="login-main">
       <div className="login-left">
@@ -18,44 +13,10 @@ const Login = () => {
             <img src={Logo} alt="Logo" />
           </div>
           <div className="login-center">
-            <h2>Hello Hải Dương</h2>
+            <h2>Du lịch Hải Dương</h2>
             <p>Welcome back! Please enter your details to continue.</p>
-            <form>
-              <input type="email" placeholder="Email" />
-              <div className="pass-input-div">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Password"
-                />
-                {showPassword ? (
-                  <FaEyeSlash onClick={() => setShowPassword(!showPassword)} />
-                ) : (
-                  <FaEye onClick={() => setShowPassword(!showPassword)} />
-                )}
-              </div>
-              <div className="login-center-options">
-                <div className="remember-div">
-                  <input type="checkbox" id="remember-checkbox" />
-                  <label htmlFor="remember-checkbox">
-                    Remember for 30 days
-                  </label>
-                </div>
-                <a href="#" className="forgot-pass-link">
-                  Forgot password?
-                </a>
-              </div>
-              <div className="login-center-buttons">
-                <button type="button">Log In</button>
-                <button type="button">
-                  <img src={GoogleSvg} alt="Google logo" />
-                  Log In with Google
-                </button>
-              </div>
-            </form>
+           
           </div>
-          <p className="login-bottom-p">
-            Don't have an account? <a href="#">Sign Up</a>
-          </p>
         </div>
       </div>
     </div>
